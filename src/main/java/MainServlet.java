1d3d37198b05d0479f7f986c969fe5c6b7a2d415
novand23 <novand23@gmail.com>
2019-10-31 20:45:07 +0200

    Initial commit

---
+++ src/main/java/MainServlet.java
@@ -0,0 +1,22 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MainServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = "Alex";
+
+        req.setAttribute("name", name);
+
+        req.getRequestDispatcher("myPages.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
---
+++ src/main/webapp/myPages.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Andrii Novak
+  Date: 31.10.2019
+  Time: 20:29
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>Hello ${name}!</h1>
+</body>
+</html>
